b4eadbca3522a1bdd9fc0708c08bcb4314b6f8bb
Maria Eduarda De Faria Braz <E711126@edp.com.br>
2025-04-22 11:43:18 -0300

    fix: ajusta erros na classe TaskRepository | feat: implementa funções na classe TaskService

--- repository/TaskRepository.py
+++ repository/TaskRepository.py
@@ -6,10 +6,10 @@ class TaskRepository:
         self.tasks = []
         self.current_id = 1
 
-    def add(self, task):
-        task.id = self.current_id
-        self.current_id += 1
+    def add(self, title, description, createdAt, updatedAt, completed=False):
+        task = Task(self.current_id, title, description, createdAt, updatedAt, completed)
         self.tasks.append(task)
+        self.current_id += 1
         return task
     
     def get_all(self):
@@ -21,11 +21,14 @@ class TaskRepository:
                 return task
         return None
         
-    def update(self, task: Task):
-        for position, existing_task in enumerate(self.tasks):
-            if existing_task.id == task.id:
-                self.tasks[position] = task
-                return task
+    def update(self, id, title, description, updatedAt, completed):
+        task = self.get_by_id(id)
+        if task:
+            task.title = title
+            task.description = description
+            task.updatedAt = updatedAt
+            task.completed = completed
+            return task
         return None
     
     def delete_all(self):
@@ -34,6 +37,5 @@ class TaskRepository:
     def delete_by_id(self, task_id: int):
         for position, task in enumerate(self.tasks):
             if task.id == task_id:
-                del self.tasks[position]
-                return task
+                return self.tasks.pop(position)
         return None
---
+++ service/TaskService.py
@@ -0,0 +1,30 @@
+import datetime
+from model import Task
+from repository import TaskRepository
+
+class TaskService:
+
+    def __init__(self):
+        self.repository = TaskRepository()
+        
+    def add(self, title, description):
+        createdAt = updatedAt = datetime.datetime.now() 
+        task = self.repository.add(task)
+        return Task(task)
+    
+    def get_all(self):
+        return self.repository.get_all()
+    
+    def get_by_id(self, id):
+        return self.repository.get_by_id(id)
+    
+    def update(self, id, title, description, completed):
+        updatedAt = datetime.datetime.now()
+        return self.repository.update(id, title, description, completed)
+    
+    def delete_all(self):
+        return self.repository.delete_all()
+    
+    def delete_by_id(self, id):
+        return self.repository.get_by_id(id)
+
